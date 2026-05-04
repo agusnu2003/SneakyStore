427fb96476a81508cff72845dbfdd91737441916
agusnu2003 <118203381+agusnu2003@users.noreply.github.com>
2026-05-04 18:52:07 -0300

    comi

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
-const CSV_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTBxqFjIMXxKJCLZwxdKZbOl6RjTZPU8_CSvTnsMAMM1AxXmSWIwRKLBeH1w8xmyINMvUFxS1fT3HDU/pub?gid=0&single=true&output=csv';
+const CSV_URL = './products.csv';
+// Google Sheets URL for reference/backup:
+// https://docs.google.com/spreadsheets/d/e/2PACX-1vTBxqFjIMXxKJCLZwxdKZbOl6RjTZPU8_CSvTnsMAMM1AxXmSWIwRKLBeH1w8xmyINMvUFxS1fT3HDU/pub?gid=0&single=true&output=csv
+
 
 document.addEventListener('DOMContentLoaded', () => {
     fetchProducts();
